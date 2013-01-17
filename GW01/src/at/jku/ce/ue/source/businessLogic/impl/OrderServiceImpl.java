ed30e2d8f671db9c626c444ddefda2c44ca9598a
andreaspfeifer <andreas.pfeifer@gmx.at>
2013-01-17 16:29:03 +0100

    priceservice added orderservice added

---
+++ GW01/src/at/jku/ce/ue/source/businessLogic/impl/OrderServiceImpl.java
@@ -0,0 +1,19 @@
+package at.jku.ce.ue.source.businessLogic.impl;
+
+import at.jku.ce.ue.source.businessLogic.OrderService;
+
+public class OrderServiceImpl implements OrderService {
+
+	@Override
+	public String placeOrder(String customerid, String producerid,
+			String partid, String inquiryid, String price, String orderId) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		
+		return null;
+	}
+
+}
---
+++ GW01/src/at/jku/ce/ue/source/businessLogic/impl/PriceServiceImpl.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package at.jku.ce.ue.source.businessLogic.impl;
+
+import at.jku.ce.ue.source.businessLogic.PriceService;
+
+/**
+ * @author andreaspfeifer
+ *
+ */
+public class PriceServiceImpl implements PriceService {
+
+	@Override
+	public int getPrice(String customerid, String producerid, String partid,
+			String inquiryid) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
