097ce04c5397354d59ded4ff9b81fa4ab9e50ade
andreaspfeifer <andreas.pfeifer@gmx.at>
2013-01-17 16:51:51 +0100

    log service changes

--- GW01/src/at/jku/ce/ue/source/businessLogic/WriteLogService.java
+++ GW01/src/at/jku/ce/ue/source/businessLogic/WriteLogService.java
@@ -4,8 +4,8 @@ public interface WriteLogService {
 
 	public void logInquiry(String customerid, String producerid, String partid, String inquiryid);
 	
-	public void logOffer(String customerid, String producerid, String partid, String price, String inquiryid, String offerid);
+	public void logOffer(String customerid, String producerid, String partid, int price, String inquiryid, String offerid);
 	
-	public void logOrder(String customerid, String producerid, String partid, String price, String inquiryid, String orderid, String offerid);
+	public void logOrder(String customerid, String producerid, String partid, int price, String inquiryid, String orderid, String offerid);
 	
 }     
